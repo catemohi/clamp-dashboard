eac4be2e8b7e1b199a06849efd556a3c58e5beef
catemohi <catemohi@localhost.localdomain>
2022-09-22 16:15:47 +0300

    gunicorn + postgre + django

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email test@test.ru
+gunicorn --bind 0.0.0.0:8000 clamp.wsgi:application 
--- src/clamp/settings.py
+++ src/clamp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#e4$0yu#kjach1=d!(cwahz#m#_6k2z^^me2z#bc9j#pojtcm('
-
+SECRET_KEY = environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["192.168.88.30",]
-
+DEBUG = bool(int(environ.get('DEBUG')))
+ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS').split()
+_ = [
+        [
+            f'{proto}{host}' for proto in ['http://', 'https://']
+        ] for host in ALLOWED_HOSTS
+    ]
+CSRF_TRUSTED_ORIGINS = [item for i in _ for item in i]
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
@@ -75,11 +79,14 @@ WSGI_APPLICATION = 'clamp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'db',
+	'PORT': 5432,
+	'USER': 'dashboard',
+	'PASSWORD': 'qwerty#dash',
+	'NAME': 'naumen_db',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -115,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
