dc30ac7cf657aeff9212385c109d143bb11d1a2b
catemohi <catemohi@gmail.com>
2022-12-13 19:05:51 +0500

    выделение задач в отдельную очередь от очереди naumen

--- src/clamp/celery.py
+++ src/clamp/celery.py
@@ -16,6 +16,9 @@ app = Celery("clamp")
 # Для получения настроек Django, связываем префикс "CELERY" с настройкой celery
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.timezone = 'Europe/Moscow'
-app.conf.task_routes = {'naumen.tasks.*': {'queue': 'naumen_crud'}}
+app.conf.task_routes = {
+    'naumen.tasks.*': {'queue': 'naumen_crud'},
+    'naumen.tasks.crud_issue': {'queue': 'celery'}
+    }
 # загрузка tasks.py в приложение django
 app.autodiscover_tasks()
