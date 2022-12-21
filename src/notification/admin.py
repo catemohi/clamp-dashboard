2102ff2f17531ba63bd5330bc713b4e5d633d61f
catemohi <catemohi@gmail.com>
2022-12-21 11:57:25 +0300

    Создание и регистрация модели уведомлений о возрате в работу

--- src/notification/admin.py
+++ src/notification/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 
 from .models import StepNotificationSetting
+from .models import RetrunToWorkNotificationSetting
 
 
 class StepNotificationSettingAdmin(admin.ModelAdmin):
 
-    list_display = ('steps', 'step_time')
-    list_display_links = ('steps', 'step_time')
-    search_fields = ('steps', 'step_time')
+    list_display = ('step', 'step_time')
+    list_display_links = ('step', 'step_time')
+    search_fields = ('step', 'step_time')
+
+
+class RetrunToWorkNotificationSettingAdmin(admin.ModelAdmin):
+
+    list_display = ('step', 'step_time')
+    list_display_links = ('step', 'step_time')
+    search_fields = ('step', 'step_time')
 
 
 admin.site.register(StepNotificationSetting, StepNotificationSettingAdmin)
+admin.site.register(RetrunToWorkNotificationSetting,
+                    RetrunToWorkNotificationSettingAdmin)
--- src/notification/models.py
+++ src/notification/models.py
@@ -7,7 +7,7 @@ class StepNotificationSetting(models.Model):
     Модель свойств и настроек для уведомлений событий по группе.
     """
 
-    steps = models.CharField(
+    step = models.CharField(
         max_length=100, verbose_name='Шаг',
         )
     step_time = models.IntegerField(
@@ -22,3 +22,28 @@ class StepNotificationSetting(models.Model):
         ordering = ('steps',)
         verbose_name = 'Настроки уведомлений шага'
         verbose_name_plural = 'Настроки уведомлений шагов'
+
+
+class RetrunToWorkNotificationSetting(models.Model):
+
+    """
+    Модель свойств и настроек для уведомлений для обращений
+    возращаемых с отложенного шага.
+    """
+
+    step = models.CharField(
+        max_length=100, verbose_name='Шаг',
+        )
+    step_time = models.IntegerField(
+        default=0,
+        verbose_name='Время за которое предупредить о возврате(сек.)',
+        db_index=True,
+        )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        ordering = ('steps',)
+        verbose_name = 'Настроки уведомлений возвращения'
+        verbose_name_plural = 'Настроки уведомлений возвращения'
