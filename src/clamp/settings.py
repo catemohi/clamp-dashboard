e37e0ea7d1fca9cf677cadb8d2a0780b06923f25
catemohi <catemohi@gmail.com>
2022-12-28 09:11:29 +0300

    поправка локали

--- src/clamp/settings.py
+++ src/clamp/settings.py
@@ -133,11 +133,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
