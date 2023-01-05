a827fe98910d93444c4704ee2c3bcd5ac06613e1
catemohi <catemohi@gmail.com>
2023-01-05 15:51:22 +0500

    fix

--- src/dashboard/tasks.py
+++ src/dashboard/tasks.py
@@ -2,7 +2,7 @@ from celery import shared_task
 
 from logging import getLogger
 
-from .services import get_day_dates_and_data
+from .services import get_day_dates_and_data, json_encoding
 from notification.services import send_report
 
 LOGGER = getLogger(__name__)
@@ -10,5 +10,7 @@ LOGGER = getLogger(__name__)
 @shared_task
 def front_params_update():
     updated_params = get_day_dates_and_data()
+    updated_params['dashboard_data'] = json_encoding(updated_params['dashboard_data'])
+    updated_params['dates'] = json_encoding(updated_params['dates'])
     send_report(updated_params)
     return True
