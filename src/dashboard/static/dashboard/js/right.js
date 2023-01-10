48ff71d253384003b72313b5e04b9908344ce80b
catemohi <catemohi@gmail.com>
2023-01-10 22:08:45 +0300

    ....

--- src/dashboard/static/dashboard/js/right.js
+++ src/dashboard/static/dashboard/js/right.js
@@ -160,7 +160,6 @@ function createNotification(){
     let updatesCollection = document.querySelectorAll('.update');
     updatesCollection.forEach(function (update) {
         let updateRawMassage = update.querySelector(".text-massage");
-        updateRawMassage = JSON.parse(updateRawMassage.textContent);
         console.log(updateRawMassage);
     });
 }
