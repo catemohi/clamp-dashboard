4a341a92a340c7440f0150bb171a900fcf4a66b6
catemohi <catemohi@gmail.com>
2023-01-11 18:46:34 +0500

    ...

--- src/dashboard/static/dashboard/js/table.js
+++ src/dashboard/static/dashboard/js/table.js
@@ -38,8 +38,8 @@ $(document).ready(function () {
             };
             $(row).children('.name').text('');
             $(row).children('.name').append('<a href=' + data.url_issue + '>'+ data.name +'</a>');
-            $(row).children('.name-service').append('<a href=' + data.url_service + '>'+ data.name +'</a>');
-            $(row).children('.name-contragent').append('<a href=' + data.url_contragent + '>'+ data.name +'</a>');
+            // $(row).children('.name-service').append('<a href=' + data.url_service + '>'+ data.name +'</a>');
+            // $(row).children('.name-contragent').append('<a href=' + data.url_contragent + '>'+ data.name +'</a>');
             $(row).children('.return-work-time').text(new Intl.DateTimeFormat("ru", options).format(new Date(data.return_to_work_time)));
             
 
