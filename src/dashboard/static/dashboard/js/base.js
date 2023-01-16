681eb4ce908161cdbac3ef7c6e2b047eda7d6002
catemohi <catemohi@gmail.com>
2023-01-16 17:47:00 +0500

    ...

--- src/dashboard/static/dashboard/js/base.js
+++ src/dashboard/static/dashboard/js/base.js
@@ -2,6 +2,8 @@ const DASHBOARD = '/dashboard/';
 const TABLE = '/table/';
 const REPORTS = '/reports/';
 const NOTIFICATION_BOX = document.querySelector('.right .recent-updates .updates');
+returnedNotificationSettings = JSON.parse(document.querySelector("#raw-burned-notification-settings").textContent);
+burnedNotificationSettings = JSON.parse(document.querySelector("#raw-returned-notification-setting").textContent);
 
 function playAudio(audioFile){
 	audioFile.play();
