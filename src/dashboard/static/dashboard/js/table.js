0db9e471d68ce79281b74a3e076929028c125d47
catemohi <catemohi@gmail.com>
2023-01-16 18:59:17 +0500

    ....

--- src/dashboard/static/dashboard/js/table.js
+++ src/dashboard/static/dashboard/js/table.js
@@ -36,12 +36,7 @@ $(document).ready(function () {
             $(row).children('.name').append('<a href=' + data.url_issue + '>'+ data.name +'</a>');
             $(row).children('.return-work-time').text(new Intl.DateTimeFormat("ru", options).format(new Date(data.return_to_work_time)));
             returnedNotificationSettings.forEach(element => {
-                console.log('Проверка: ', element);
-                console.log('Element step', element.step);
-                console.log('Data step', data.step);
                 if (element.step === data.step) {
-                    console.log(data.step_time);
-                    console.log(element.alarm_time);
                     if (data.step_time < element.alarm_time){
                         $(row).css('background-color', '#ff7B7B');
                     } else {
@@ -51,12 +46,12 @@ $(document).ready(function () {
             });
             burnedNotificationSettings.forEach(element => {
                 console.log('Проверка: ', element);
-                console.log('Element step', element.step);
-                console.log('Data step', data.step);
                 if (element.step === data.step) {
-                    console.log(data.step_time);
-                    console.log(element.step_time - element.alarm_time);
-                    if (data.step_time > element.step_time - element.alarm_time){
+                    let timedelta = element.step_time - element.alarm_time;
+                    console.log('Timedelta: ', timedelta);
+                    console.log('Timedelta: ', timedelta);
+                    console.log('Issue step time: ', data.step_time);
+                    if (data.step_time > timedelta){
                         $(row).css('background-color', '#ff7B7B');
                     } else {
                         $(row).css('background-color', 'transparent');
