a40b97c773ce5491b3c765e29a6d1e6fc863932b
catemohi <catemohi@gmail.com>
2023-01-20 12:25:14 +0300

    ...

--- src/clamp/settings.py
+++ src/clamp/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import ldap
-
 from os import environ
 from pathlib import Path
 from json import load
