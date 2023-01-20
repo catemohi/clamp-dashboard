9bce22c609df6a7036a09ab9f923cf61aeb34b92
catemohi <catemohi@gmail.com>
2023-01-20 22:58:15 +0300

    ..

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
 
 from .views import index_page, login_page, logout_page
 from .views import dashboard_page, table_page, reports_page
@@ -20,4 +18,4 @@ urlpatterns = [
     path('json/reports', report_json, name='json/reports'),
     ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
