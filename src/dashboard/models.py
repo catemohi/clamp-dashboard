3e2c0968d125d01d31d839e883aff75a4c527849
catemohi <catemohi@gmail.com>
2023-01-23 21:34:28 +0500

    ...

--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -141,6 +141,7 @@ class Profile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        instance.profile.save()
 
 
 @receiver(post_save, sender=User)
