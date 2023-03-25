c442ff547d19a29ca543f593f203213175c8d740
catemohi <catemohi@gmail.com>
2023-03-25 13:24:15 +0300

    ..

--- src/telegram_bot/services.py
+++ src/telegram_bot/services.py
@@ -255,7 +255,7 @@ def replace_for_markdown(data: str) -> str:
     Returns:
         str: экранированный текст
     """
-    pattern = r'[-.+?^$[\](){}><!]'
+    pattern = r'[-.+?^$[\](){}><!=]'
     list_after = []
     for line in data.split('\n'):
         if 'http' in line:
