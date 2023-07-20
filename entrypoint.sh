432e5096e27cee0e08f302604d1ac372f64d5d27
catemohi <catemohi@gmail.com>
2023-07-21 00:43:16 +0500

    Оптимизация docker compose

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email test@test.ru
-gunicorn --bind 0.0.0.0:8000 clamp.wsgi:application & \
-daphne -b 0.0.0.0 -p 8001 clamp.asgi:application
--- worker.entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-celery -A clamp worker -l info -c 1 -Q naumen_crud & \
-celery -A clamp worker -l info -c 10 -Q celery
